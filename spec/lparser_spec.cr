289f3fbf0b028fbe2e174f303afd559f7f5bee3a
Marwin Acker <marwin@funk.net>
2024-10-04 11:10:08 +0200

    complex test for defun

--- spec/lparser_spec.cr
+++ spec/lparser_spec.cr
@@ -14,11 +14,34 @@ describe LParser do
     LParser.parse("'(\"hello\")").should eq [LList.new ([LString.new("hello")] of LData)]
   end
   it "parses defun expression" do
-    LParser.parse("(defun (fnname x z y) (lambda (a b c) (+ a b c x z y)))").should eq [
-      LExpression.new(LRef.new("defun"), [
-        LExpression.new(LRef.new("fnname"), [LRef.new("x"), LRef.new("y"), LRef.new("z")] of LData),
-        LExpression.new(LRef.new("lambda"), [LRef.new("a"), LRef.new("b"), LRef.new("c")] of LData)
-      ] of LData)
-      ]
+    result = LParser.parse("(defun (fnname x z y) (lambda (a b c) (+ a b c x z y)))")[0].as LExpression
+    result.first.should eq LRef.new("defun")
+    (result.arguments[0].as LExpression).first.should eq LRef.new("fnname")
+    (result.arguments[0].as LExpression).arguments.should eq ([LRef.new("x"), LRef.new("z"), LRef.new("y")] of LData)
+    (result.arguments[1].as LExpression).first.should eq LRef.new("lambda")
+    ((result.arguments[1].as LExpression).arguments[0].as LExpression).first.should eq LRef.new("a")
+    ((result.arguments[1].as LExpression).arguments[0].as LExpression).arguments.should eq ([LRef.new("b"), LRef.new("c")] of LData)
+    ((result.arguments[1].as LExpression).arguments[1].as LExpression).first.should eq LRef.new("+")
+    ((result.arguments[1].as LExpression).arguments[1].as LExpression).arguments.should eq ([LRef.new("a"), LRef.new("b"), LRef.new("c"), LRef.new("x"), LRef.new("z"), LRef.new("y")] of LData)
+
+
+    #result.should eq LExpression.new(LRef.new("defun"), [
+    #    LExpression.new(LRef.new("fnname"), [LRef.new("x"), LRef.new("z"), LRef.new("y")] of LData),
+    #    LExpression.new(LRef.new("lambda"), [
+    #      LExpression.new(LRef.new("a"), [
+    #        LRef.new("b"),
+    #        LRef.new("c")
+    #      ] of LData),
+    #      LExpression.new(LRef.new("+"), [
+    #        LRef.new("a"),
+    #        LRef.new("b"),
+    #        LRef.new("c"),
+    #        LRef.new("x"),
+    #        LRef.new("z"),
+    #        LRef.new("y")
+    #      ] of LData)
+    #  ] of LData),
+    #  ] of LData)
+
   end
 end
